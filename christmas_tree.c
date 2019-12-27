5d92e911cf409cce18c7df83e9ead029ee7be8ab
Mike Mallin <mike@mremallin.ca>
2019-12-26 20:04:10 -0500

    SDL Working

--- christmas_tree.c
+++ christmas_tree.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+#include <SDL2/SDL.h>
+
 int
 main (int argc, char *argv[])
 {
+	int rc = 0;
+
+	rc = SDL_Init(SDL_INIT_VIDEO);
+	if (rc != 0) {
+		fprintf(stderr, "SDL_Init failed (%u): %s\n", rc, SDL_GetError());
+		exit(rc);
+	}
+
 	printf("Christmas tree goes here!\n");
+
+	SDL_Quit();
 	return 0;
 }
