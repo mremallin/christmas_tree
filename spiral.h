fe44abfa788130e7b37244e748b470c9b2396afe
Mike Mallin <mike@mremallin.ca>
2020-01-09 22:43:50 -0500

    Convert sprial.h to 4-space indentation

--- spiral.h
+++ spiral.h
@@ -15,20 +15,20 @@
 typedef void *spiral;
 
 typedef struct spiral_init_ctx_ {
-	/** The number of points making up a given spiral. */
-	int num_slices;
-	/** Number of rotations to complete before intersecting with y_max */
-	int num_rotations;
-	/** How long for a given point to travel from 0 -> y_max */
-	int cycle_time_ms;
-	/** OpenGL coordinate for y_max */
-	float y_max;
-	/** Slope of the branches */
-	float slope;
-	/** Offset in radians around the y-axis */
-	float starting_angle_offset;
-	/** {r,g,b,a} of the given spiral */
-	vec4 color;
+    /** The number of points making up a given spiral. */
+    int num_slices;
+    /** Number of rotations to complete before intersecting with y_max */
+    int num_rotations;
+    /** How long for a given point to travel from 0 -> y_max */
+    int cycle_time_ms;
+    /** OpenGL coordinate for y_max */
+    float y_max;
+    /** Slope of the branches */
+    float slope;
+    /** Offset in radians around the y-axis */
+    float starting_angle_offset;
+    /** {r,g,b,a} of the given spiral */
+    vec4 color;
 } spiral_init_ctx;
 
 /** Initializes a spiral */
